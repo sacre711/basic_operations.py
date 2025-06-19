fbddb6a4ff611a2d9363929d9f2ce71c7ff65640
Sacre Tshibanda <122829214+sacre711@users.noreply.github.com>
2025-06-19 13:59:20 +0200

    Create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+global  number1 = 10 # value assigned to the first variable
+global number2 = 5 # value assigned to the second variable
+addition = number1 + number2
+subtraction = number1 - number2
+quotient = number1 * number2
+
+# Addition of 10 and 5 is 15
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50 
+print("Addition of ", number1, " and ", number2, " is ", addition)
+print("Subtraction of ", number1, " and ", number2, " is ", subtraction)
+print("Multiplication of ", number1, " and ", number2, " is ", quotient)
